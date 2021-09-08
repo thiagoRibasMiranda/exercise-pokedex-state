62e1916a0899efe5b8eb69a647b41e9dd9c4e2a4
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-09-08 10:42:24 -0300

    Requirement 1/2

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import Pokemon from './Pokemon';
+// import 
 
 class Pokedex extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            n: 0,
+        };
+
+        this.cont = this.cont.bind(this)
+    }
+
+    cont () {
+        if(this.state.n >= this.props.pokemons.length) {
+            this.setState(({n}) => ({
+                n: n * 0,
+            }))
+        } else {
+            this.setState(({n}) => ({
+                n: n + 1
+            }))
+        }
+        
+    }
     render() {
         return (
             <div className="pokedex">
                 {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+                <Pokemon key={this.props.pokemons[this.state.n].id} pokemon={this.props.pokemons[this.state.n]}/>
+                <button onClick={ this.cont }>Próximo pokémon</button>
             </div>
         );
     }
